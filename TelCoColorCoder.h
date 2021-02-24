8ebb95fc0d19017ba5afe686f3001fbe4d19ad12
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-02-24 21:07:22 +0530

    Update TelCoColorCoder.h

--- TelCoColorCoder.h
+++ TelCoColorCoder.h
@@ -1,22 +1,18 @@
 #ifndef TELCOCOLORCODER
 #define TELCOCOLORCODER
+
 #include<iostream>
 #include<string>
+
 namespace TelCoColorCoder
 {
 	enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
 	enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-	const char* MajorColorNames[] = {
-		"White", "Red", "Black", "Yellow", "Violet"
-	};
-	int numberOfMajorColors =
-		sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-	const char* MinorColorNames[] = {
-		"Blue", "Orange", "Green", "Brown", "Slate"
-	};
-	int numberOfMinorColors =
-		sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+	extern const int numberOfMajorColors;
+	extern const int numberOfMinorColors; 
+	
+
 
 	class ColorPair {
 	private:
