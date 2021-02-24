445243ff98f02a6c5b5ef76e508d657afb89dd3e
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-02-24 22:29:05 +0530

    Update TelCoColorCoder_Test.cpp

--- TelCoColorCoder_Test.cpp
+++ TelCoColorCoder_Test.cpp
@@ -1,25 +1,28 @@
 #include "TelCoColorCoder.h"
 #include<assert.h>
 
-void testNumberToPair(int pairNumber,
-	TelCoColorCoder::MajorColor expectedMajor,
-	TelCoColorCoder::MinorColor expectedMinor)
+void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor)
 {
-	TelCoColorCoder::ColorPair colorPair =
-		TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+	TelCoColorCoder::ColorPair colorPair =TelCoColorCoder::GetColorFromPairNumber(pairNumber);
 	std::cout << "Got pair " << colorPair.ToString() << std::endl;
 	assert(colorPair.getMajor() == expectedMajor);
 	assert(colorPair.getMinor() == expectedMinor);
 }
 
-void testPairToNumber(
-	TelCoColorCoder::MajorColor major,
-	TelCoColorCoder::MinorColor minor,
-	int expectedPairNumber)
+void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber)
 {
 	int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
 	std::cout << "Got pair number " << pairNumber << std::endl;
 	assert(pairNumber == expectedPairNumber);
+}
+void printPairColorCode()
+{
+	int totalColorPairCount = TelCoColorCoder::numberOfMajorColors * TelCoColorCoder::numberOfMinorColors;
+	for (int pairNumber = 1 ; pairNumber <= totalColorPairCount ; ++ pairNumber )
+	{
+		TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+		std::cout <<std::setw(6)<< pairNumber << " \t   | \t "<< colorPair.ToString() << std::endl ;
+	}
 }
 
 int main() {
