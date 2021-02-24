92d152240a3e882bc90b1463a4181e0e91bfac19
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-02-24 17:06:19 +0530

    Update TelCoColorCoder.h

--- TelCoColorCoder.h
+++ TelCoColorCoder.h
@@ -1,26 +1,34 @@
+#ifndef TELCOCOLORCODER
+#define TELCOCOLORCODER
 #include<iostream>
 #include<string>
-using namespace std;
-
 namespace TelCoColorCoder
 {
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+	enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+	enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
+	const char* MajorColorNames[] = {
+		"White", "Red", "Black", "Yellow", "Violet"
+	};
+	int numberOfMajorColors =
+		sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+	const char* MinorColorNames[] = {
+		"Blue", "Orange", "Green", "Brown", "Slate"
+	};
+	int numberOfMinorColors =
+		sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-class ColorPair {
-        private:
-            MajorColor majorColor;
-            MinorColor minorColor;
-        public:
-	    ColorPair();
-            ColorPair(MajorColor major, MinorColor minor);
-            MajorColor getMajor();
-            MinorColor getMinor();
-            std::string ToString();
-	    ColorPair GetColorFromPairNumber(int pairNumber);
-	    void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
-	    void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber);
-			
-    };
+	class ColorPair {
+	private:
+		MajorColor majorColor;
+		MinorColor minorColor;
+	public:
+		ColorPair(MajorColor major, MinorColor minor);
+
+		MajorColor getMajor();
+		MinorColor getMinor();
+		std::string ToString();
+	};
+	ColorPair GetColorFromPairNumber(int pairNumber);
+	int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 }
